d424268e953e7ae579d52292867a2c9024740eb2
Matthew Hillier <MHillier98@users.noreply.github.com>
2019-08-19 00:28:45 +1000

    Updated scoring, sprite animation, powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int score = 0;
+    public int startingScore = 0;
+    private int score = 0;
+
+    public int dotScore = 10;
+    public int largeDotScore = 50;
+
+    public bool poweredUp = false;
+    public int poweredUpTime = 6;
+
+    SpriteRenderer spriteRenderer;
+
 
     void Start()
     {
-        score = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        score = startingScore;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
+            spriteRenderer.flipY = false;
             transform.eulerAngles = new Vector3(0, 0, 90);
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            spriteRenderer.flipY = true;
             transform.eulerAngles = new Vector3(0, 0, 180);
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
+            spriteRenderer.flipY = false;
             transform.eulerAngles = new Vector3(0, 0, 270);
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
+            spriteRenderer.flipY = false;
             transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // i could set the dots to not be active, if i want to reactivate them for new levels
+        if (collision.tag.Equals("Dots"))
+        {
+            Destroy(collision.gameObject);
+            // collision.gameObject.SetActive(false);
+            AddScore(dotScore);
+        }
+        else if (collision.tag.Equals("Large Dots"))
+        {
+            Destroy(collision.gameObject);
+            // collision.gameObject.SetActive(false);
+            AddScore(largeDotScore);
+            PowerUp();
+            Invoke("PowerDown", poweredUpTime);
         }
     }
 
@@ -51,22 +85,13 @@ public class PlayerController : MonoBehaviour
         return score;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void PowerUp()
     {
-        // Debug.Log(collision.name);
+        poweredUp = true;
+    }
 
-        // I can set the dots to not be active, if i want to reactivate them for new levels
-        if (collision.tag.Equals("Dots"))
-        {
-            Destroy(collision.gameObject);
-            // collision.gameObject.SetActive(false);
-            AddScore(10);
-        }
-        else if (collision.tag.Equals("Large Dots"))
-        {
-            Destroy(collision.gameObject);
-            // collision.gameObject.SetActive(false);
-            AddScore(50);
-        }
+    public void PowerDown()
+    {
+        poweredUp = false;
     }
 }
