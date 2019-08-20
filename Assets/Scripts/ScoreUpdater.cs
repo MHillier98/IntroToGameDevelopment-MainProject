5eb4bd32baa25b450e36dbaea1b90256d803668f
Matthew Hillier <MHillier98@users.noreply.github.com>
2019-08-21 00:20:59 +1000

    Added scorer script + high score saving

---
+++ Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    public bool isHighScorer = false;
+
+    [SerializeField]
+    private int highScore = 0;
+
+    private TextMeshProUGUI scorerText;
+    private PlayerController playerController;
+
+    void Start()
+    {
+        scorerText = GetComponent<TextMeshProUGUI>();
+        playerController = GameObject.FindGameObjectWithTag("Ms Pac-Man").gameObject.GetComponent<PlayerController>();
+
+        highScore = PlayerPrefs.GetInt("High Score", highScore);
+
+        if (isHighScorer)
+        {
+            scorerText.text = highScore.ToString();
+        }
+    }
+
+    void Update()
+    {
+        int playerScore = playerController.GetScore();
+        if (isHighScorer)
+        {
+            if (playerScore > highScore)
+            {
+                highScore = playerScore;
+                PlayerPrefs.SetInt("High Score", highScore);
+                scorerText.text = playerScore.ToString();
+            }
+        }
+        else
+        {
+            scorerText.text = playerScore.ToString();
+        }
+    }
+}
